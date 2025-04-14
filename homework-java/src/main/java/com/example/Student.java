8d8195c85131b083bb9b80104ff4e5af8a88de96
Gab Caser <caserogabriela@gmail.com>
2025-04-14 17:39:24 +0200

    Class Student created

---
+++ homework-java/src/main/java/com/example/Student.java
@@ -0,0 +1,76 @@
+package com.example;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class Student {
+    private static final Set<String> usedIds = new HashSet<>();
+
+    private final String studentId;
+    private String name;
+    private String address;
+    private String email;
+    private Course course;
+
+
+    public Student(String name,String address,String email) {
+        this.studentId = generateUniqueStudentId();
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.course = null;
+    }
+    public Student(){
+        this.studentId = generateUniqueStudentId();
+    }
+
+
+    private String generateUniqueStudentId(){
+        String id= "";
+        do{
+            id=UUID.randomUUID().toString();
+        } while (usedIds.contains(id));
+        usedIds.add(id);
+        return id;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (!name.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+")) {
+            throw new IllegalArgumentException("El nombre no puede contener números ni caracteres especiales.");
+        }
+        this.name = name;
+    }
+}
+
